385213c4af37cf9451c7c0bf28c173071497ca4c
Damjan Vujnovic <damjan@samuraiprinciple.com>
2013-04-19 09:20:30 +0100

    upgraded jquery and kineticjs

--- dl_dependencies.sh
+++ dl_dependencies.sh
@@ -2,9 +2,9 @@ rm -rf lib
 
 mkdir -p lib
 
-curl http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js > lib/jquery-1.9.0.js
+curl http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js > lib/jquery-1.9.1.js
 curl http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.3/underscore-min.js > lib/underscore-1.4.3-min.js
-curl http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.4.1.js > lib/kinetic-v4.4.1.js
+curl http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.4.3.js > lib/kinetic-v4.4.3.js
 curl https://raw.github.com/brandonaaron/jquery-mousewheel/master/jquery.mousewheel.js > lib/jquery.mousewheel.js
 version=`grep Version  lib/jquery.mousewheel.js | tr -d -C "0-9."`
 mv lib/jquery.mousewheel.js lib/jquery.mousewheel-$version.js
