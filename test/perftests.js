1d8d24131c3caf916c585aa524509b253c07fe0f
Gojko Adzic <gojko@gojko.com>
2013-07-09 09:24:31 +0100

    pulled out perftests into a separate file, added a test for selection

---
+++ test/perftests.js
@@ -0,0 +1,77 @@
+/*global jQuery, window, mapModel, Kinetic */
+(function () {
+	'use strict';
+	window.startPerformanceTest = function (test, mapMultiplication, multiplicationType) {
+		test = test || 'fold';
+		var cycle = function (callback) {
+				var cycleText = jQuery('#cyclerate'),
+					cycles = 0,
+					cycleTime = 0,
+					interval = setInterval(function () {
+						var start = Date.now();
+						callback(cycles);
+						cycleTime += Date.now() - start;
+						cycles++;
+						if (cycles % 30) {
+							cycleText.text((cycles * 1000 / cycleTime).toFixed(2));
+						}
+					}, 100);
+				console.log('run clearInterval(' + interval + ') to stop');
+			},
+			tests = {
+				fold: function () {
+					mapModel.selectNode(1);
+					cycle(function () {
+						window.mapModel.toggleCollapse();
+					});
+				},
+				move: function () {
+					mapModel.selectNode(1);
+					mapModel.selectNodeLeft();
+					cycle(function (cycles) {
+						if ((cycles / 7).toFixed(0) % 2) {
+							window.mapModel.moveUp();
+						} else {
+							window.mapModel.moveDown();
+						}
+					});
+				},
+				scroll: function () {
+					new Kinetic.Tween({node: jQuery('#container').data('mm-stage'), x: 1000, y: 400, duration: 10}).play();
+				},
+				select: function () {
+					mapModel.selectNode(1);
+					cycle(function (cycles) {
+						mapModel.activateLevel('test', (cycles % 4) + 1);
+					});
+				},
+			},
+			i;
+		if (mapMultiplication) {
+			mapModel.copy();
+			for (i = 0; i < mapMultiplication; i++) {
+				if (multiplicationType === 'center') {
+					mapModel.selectNode(1);
+				}
+				mapModel.paste();
+			}
+		}
+		tests[test]();
+	};
+	var oldRequestAnimationFrame = Kinetic.Animation.requestAnimFrame,
+		framerateText = jQuery('#framerate'),
+		t0 = Date.now(),
+		frames = 0;
+	Kinetic.Animation.requestAnimFrame = function () {
+		var lapsed = Date.now() - t0;
+		frames++;
+		if (oldRequestAnimationFrame) {
+			oldRequestAnimationFrame.apply(window, arguments);
+		}
+		if (lapsed > 3000) {
+			framerateText.text((frames * 1000 / lapsed).toFixed(2));
+			frames = 0;
+			t0 = Date.now();
+		}
+	};
+}());
