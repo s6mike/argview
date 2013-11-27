966fe01150ff67988ee84e77e034117841460ef8
Gojko Adzic <gojko@gojko.com>
2013-11-27 16:56:48 +0000

    drop widget now supports drag & drop from web pages

--- src/image-drop-widget.js
+++ src/image-drop-widget.js
@@ -1,4 +1,4 @@
-/*global $, FileReader, Image, MAPJS */
+/*global $, FileReader, Image, MAPJS, _ */
 MAPJS.ImageInsertController = function (callback) {
 	'use strict';
 	var self = this,
@@ -14,18 +14,25 @@ MAPJS.ImageInsertController = function (callback) {
 			return loader.promise();
 		},
 		domImg;
+	self.insertDataUrl = function (dataUrl, evt) {
+		domImg = new Image();
+		domImg.onload = function () {
+			callback(dataUrl, domImg.width, domImg.height, evt);
+		};
+		domImg.src = dataUrl;
+	};
 	self.insertFiles = function (files, evt) {
 		$.each(files, function (idx, fileInfo) {
 			if (/^image\//.test(fileInfo.type)) {
-				$.when(readFileIntoDataUrl(fileInfo)).done(function (dataUrl) {
-					domImg = new Image();
-					domImg.onload = function () {
-						callback(dataUrl, domImg.width, domImg.height, evt);
-					};
-					domImg.src = dataUrl;
-				});
+				$.when(readFileIntoDataUrl(fileInfo)).done(function (dataUrl) { self.insertDataUrl(dataUrl, evt); });
 			}
 		});
+	};
+	self.insertHtmlContent = function (htmlContent, evt) {
+		var images = htmlContent.match(/img[^>]*src="([^"]*)"/);
+		if (images && images.length > 0) {
+			_.each(images.slice(1), function (dataUrl) { self.insertDataUrl(dataUrl, evt); });
+		}
 	};
 };
 $.fn.imageDropWidget = function (imageInsertController) {
@@ -35,11 +42,15 @@ $.fn.imageDropWidget = function (imageInsertController) {
 			return false;
 		}
 	}).on('drop', function (e) {
-		var dataTransfer = e.originalEvent.dataTransfer;
+		var dataTransfer = e.originalEvent.dataTransfer,
+			htmlContent;
 		e.stopPropagation();
 		e.preventDefault();
 		if (dataTransfer && dataTransfer.files && dataTransfer.files.length > 0) {
 			imageInsertController.insertFiles(dataTransfer.files, e.originalEvent);
+		} else if (dataTransfer) {
+			htmlContent = dataTransfer.getData('text/html');
+			imageInsertController.insertHtmlContent(htmlContent, e.originalEvent);
 		}
 	});
 	return this;
