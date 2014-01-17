c485d5330c3cc8ce60af855a74360b45268c369d
Gojko Adzic <gojko@gojko.com>
2014-01-17 15:28:14 +0000

    prevent updateStyle roundtrip

--- src/map-toolbar-widget.js
+++ src/map-toolbar-widget.js
@@ -5,11 +5,13 @@ jQuery.fn.mapToolbarWidget = function (mapModel) {
 			'copy', 'cut', 'paste', 'resetView', 'openAttachment', 'toggleAddLinkMode', 'activateChildren', 'activateNodeAndChildren', 'activateSiblingNodes', 'editIcon'],
 		changeMethodNames = ['updateStyle'];
 	return this.each(function () {
-		var element = jQuery(this);
+		var element = jQuery(this), preventRoundtrip = false;
 		mapModel.addEventListener('nodeSelectionChanged', function () {
+			preventRoundtrip = true;
 			element.find('.updateStyle[data-mm-target-property]').val(function () {
 				return mapModel.getSelectedStyle(jQuery(this).data('mm-target-property'));
 			}).change();
+			preventRoundtrip = false;
 		});
 		mapModel.addEventListener('addLinkModeToggled', function () {
 			element.find('.toggleAddLinkMode').toggleClass('active');
@@ -23,6 +25,9 @@ jQuery.fn.mapToolbarWidget = function (mapModel) {
 		});
 		changeMethodNames.forEach(function (methodName) {
 			element.find('.' + methodName).change(function () {
+				if (preventRoundtrip) {
+					return;
+				}
 				var tool = jQuery(this);
 				if (tool.data('mm-target-property')) {
 					mapModel[methodName]('toolbar', tool.data('mm-target-property'), tool.val());
