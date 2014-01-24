d28ac0c059c8d74eba0d479f79c4b8dd3cf62917
David de Florinier <dave@deflorinier.me.uk>
2014-01-24 15:12:27 +0000

    observable dispatch event should survive exceptions

--- src/observable.js
+++ src/observable.js
@@ -1,3 +1,4 @@
+/*global console*/
 var observable = function (base) {
 	'use strict';
 	var listeners = [];
@@ -34,7 +35,12 @@ var observable = function (base) {
 				return secondListenerDetails.priority - firstListenerDetails.priority;
 			})
 			.some(function (listenerDetails) {
-				return listenerDetails.listener.apply(undefined, args) === false;
+				try {
+					return listenerDetails.listener.apply(undefined, args) === false;
+				} catch (e) {
+					console.log('dispatchEvent failed', e, listenerDetails);
+				}
+
 			});
 	};
 	return base;
