0449868d919f685fc4d4fc71d54f26e2a234df6b
Gojko Adzic <gojko@gojko.com>
2014-04-12 17:03:51 +0100

    tests for DOMRender

--- src/dom-map-widget.js
+++ src/dom-map-widget.js
@@ -1,42 +1,6 @@
 /*jslint nomen: true, newcap: true, browser: true*/
 /*global MAPJS, $, Hammer, _, jQuery*/
-MAPJS.DOMRender = {
-	nodeCacheMark: function (idea) {
-		'use strict';
-		return {
-			title: idea.title,
-			icon: idea.attr && idea.attr.icon && _.pick(idea.attr.icon, 'width', 'height', 'position'),
-			collapsed: idea.attr && idea.attr.collapsed
-		};
-	},
-	addNodeCacheMark: function (domNode, idea) {
-		'use strict';
-		domNode.data('nodeCacheMark', MAPJS.DOMRender.nodeCacheMark(idea));
-	},
-	dimensionProvider: function (idea) {
-		'use strict'; /* support multiple stages? */
-		var existing = document.getElementById('node_' + idea.id),
-			textBox,
-			result;
-		if (existing) {
-			textBox = $(existing);
-			if (_.isEqual(textBox.data('nodeCacheMark'), MAPJS.DOMRender.nodeCacheMark(idea))) {
-				return _.pick(textBox.data(), 'width', 'height');
-			}
-		}
-		textBox = $('<div>').addClass('mapjs-node').css({position: 'absolute', visibility: 'hidden'}).appendTo('body').updateNodeContent(idea);
-		result = {
-			width: textBox.outerWidth(true),
-			height: textBox.outerHeight(true)
-		};
-		textBox.detach();
-		return result;
-	},
-	layoutCalculator: function (contentAggregate) {
-		'use strict';
-		return MAPJS.calculateLayout(contentAggregate, MAPJS.DOMRender.dimensionProvider);
-	}
-};
+
 $.fn.queueFadeOut = function () {
 	'use strict';
 	var element = $(this);
