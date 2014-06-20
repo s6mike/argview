45d2c291a8e126014ce9c84960c9eea0142eb9d6
Gojko Adzic <gojko@gojko.com>
2014-06-20 10:19:25 +0100

    removed kinetic

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 mkdir -p pub
 cat src/mapjs.js src/observable.js src/url-helper.js src/content.js src/layout.js src/clipboard.js src/hammer-draggable.js src/map-model.js src/drag-and-drop.js src/map-toolbar-widget.js src/link-edit-widget.js src/image-drop-widget.js src/dom-map-view.js src/dom-map-widget.js > pub/mapjs-compiled.js
-cat kinetic-src/kinetic-v4.5.4.min.js kinetic-src/jquery.mousewheel-3.1.3.js kinetic-src/kinetic.connector.js kinetic-src/kinetic.link.js kinetic-src/kinetic.clip.js kinetic-src/kinetic.idea.js kinetic-src/kinetic-mediator.js kinetic-src/map-widget.js > pub/kinetic-mapjs-compiled.js
 if [ -d "../mindmup/public" ]; then
  cp pub/mapjs-compiled.js ../mindmup/public
- cp pub/kinetic-mapjs-compiled.js ../mindmup/public/e
 fi
--- dl_dependencies.sh
+++ dl_dependencies.sh
@@ -4,11 +4,9 @@ mkdir -p lib
 
 curl http://mindmup.s3.amazonaws.com/lib/jquery-2.0.2.min.js > lib/jquery-2.0.2.min.js
 curl http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore.js > lib/underscore-1.4.4.js
-curl http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.5.4.js > lib/kinetic-v4.5.4.js
 curl http://static.mindmup.com/lib/jquery.mousewheel-3.1.3.js > lib/jquery.mousewheel-3.1.3.js
 curl http://mindmup.s3.amazonaws.com/lib/jquery.hotkeys.js > lib/jquery.hotkeys.js
 curl http://cloud.github.com/downloads/harthur/color/color-0.4.1.min.js > lib/color-0.4.1.min.js
 curl http://mindmup.s3.amazonaws.com/lib/hammer.min-1.1.3.js > lib/jquery.hammer.min.js
 curl http://static.mindmup.com.s3.amazonaws.com/lib/hammer-1.1.3.min.js > lib/hammer.min.js
 curl http://static.mindmup.com.s3.amazonaws.com/lib/jquery.hammer-1.1.3.min.js > lib/jquery.hammer.min.js
-curl http://static.mindmup.com/lib/jquery-ui-1.10.0.custom.min.js > lib/jquery-ui-1.10.0.custom.min.js
