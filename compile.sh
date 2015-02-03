04d26bc53590bf6bc88f4ad8fd32b70e036270f4
David de Florinier <dave@deflorinier.me.uk>
2015-02-03 09:09:58 +0000

    siimplify compile.sh

--- compile.sh
+++ compile.sh
@@ -3,12 +3,11 @@ rc=$?
 if [[ $rc != 0 ]] ; then
   echo "grunt failed, bailing out"
   exit $rc
-else
-	echo 'compiling'
-	mkdir -p pub
-	cat src/mapjs.js src/observable.js src/url-helper.js src/content.js src/layout.js src/clipboard.js src/hammer-draggable.js src/map-model.js src/map-toolbar-widget.js src/link-edit-widget.js src/image-drop-widget.js src/dom-map-view.js src/dom-map-widget.js > pub/mapjs-compiled.js
-	if [ -d "../mindmup/public" ]; then
-	 cp pub/mapjs-compiled.js ../mindmup/public
-	 echo 'compiled to ../mindmup/public'
-	fi
+fi
+echo 'compiling'
+mkdir -p pub
+cat src/mapjs.js src/observable.js src/url-helper.js src/content.js src/layout.js src/clipboard.js src/hammer-draggable.js src/map-model.js src/map-toolbar-widget.js src/link-edit-widget.js src/image-drop-widget.js src/dom-map-view.js src/dom-map-widget.js > pub/mapjs-compiled.js
+if [ -d "../mindmup/public" ]; then
+ cp pub/mapjs-compiled.js ../mindmup/public
+ echo 'compiled to ../mindmup/public'
 fi
