c378b73ed03d56feafa90e04d86f2dcb431d5f8b
Gojko Adzic <gojko@gojko.com>
2015-02-22 05:51:09 +0100

    package json used to install npm dependencies

--- GruntFile.js
+++ GruntFile.js
@@ -9,11 +9,8 @@ install grunt cli:
 	npm install -g grunt-cli
 
 - per project
-npm install grunt-contrib-jasmine --save-dev
-npm install grunt-notify --save-dev
-npm install grunt-contrib-watch --save-dev
-npm install grunt-jscs --save-dev
-npm install grunt-contrib-jshint --save-dev
+
+npm install
 
 */
 
