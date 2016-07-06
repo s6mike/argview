6f9d2052001441f7878cec3da9f07e1fdb11a225
David de Florinier <dave@deflorinier.me.uk>
2016-07-06 08:26:47 +0100

    rebuild

--- dist/mindmup-mapjs.js
+++ dist/mindmup-mapjs.js
@@ -660,10 +660,15 @@ MAPJS.content = function (contentAggregate, sessionKey) {
 		}
 		return false;
 	};
-	contentAggregate.insertIntermediateMultiple = function (idArray) {
+	contentAggregate.insertIntermediateMultiple = function (idArray, ideaOptions) {
 		var newId;
 		contentAggregate.startBatch();
-		newId = contentAggregate.insertIntermediate(idArray[0]);
+		newId = contentAggregate.insertIntermediate(idArray[0], ideaOptions && ideaOptions.title);
+		if (ideaOptions && ideaOptions.attr) {
+			Object.keys(ideaOptions.attr).forEach(function (key) {
+				contentAggregate.updateAttr(newId, key, ideaOptions.attr[key]);
+			});
+		}
 		_.each(idArray.slice(1), function (id) {
 			contentAggregate.changeParent(id, newId);
 		});
@@ -1630,6 +1635,21 @@ MAPJS.MapModel = function (layoutCalculatorArg, selectAllTitles, clipboardProvid
 			}
 		}
 
+	};
+	this.insertIntermediateGroup = function (source, options) {
+		var activeNodes = [],
+			group = (options && options.group) || true;
+		if (!isEditingEnabled) {
+			return false;
+		}
+		if (!isInputEnabled || currentlySelectedIdeaId === idea.id) {
+			return false;
+		}
+		analytic('insertIntermediate', source);
+		self.applyToActivated(function (i) {
+			activeNodes.push(i);
+		});
+		idea.insertIntermediateMultiple(activeNodes, { title: 'group', attr: {group: group, contentLocked: true}});
 	};
 	this.insertIntermediate = function (source) {
 		var activeNodes = [], newId;
