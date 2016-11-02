629ec48bafa088acef12d4c204d3a97bcc0b19df
David de Florinier <dave@deflorinier.me.uk>
2016-11-02 14:58:49 +0000

    node-resize-widget-spec

--- test/node-resize-widget-spec.js
+++ test/node-resize-widget-spec.js
@@ -1,7 +1,16 @@
 /*global beforeEach, describe, afterEach, expect, it, jQuery, jasmine, spyOn*/
 describe('nodeResizeWidget', function () {
 	'use strict';
-	var element, mapModel, stagePositionForPointEvent, underTest;
+	var element, mapModel, stagePositionForPointEvent, underTest, textElement,
+		eventForX = function (x) {
+			return {
+				x: x,
+				stopPropagation: jasmine.createSpy('stopPropagation'),
+				gesture: {
+					stopPropagation: jasmine.createSpy('stopGesturePropagation')
+				}
+			};
+		};
 	beforeEach(function () {
 		mapModel = jasmine.createSpyObj('mapModel', ['selectNode']);
 		stagePositionForPointEvent = jasmine.createSpy('stagePositionForPointEvent').and.callFake(function (evt) {
@@ -12,12 +21,13 @@ describe('nodeResizeWidget', function () {
 			'<div>' +
 				'<span>Some</span>' +
 			'</div>'
-		).css({'min-width': 100});
-		element.find('span').css({'min-width': 50, 'max-width': 60, 'display': 'inline-block'});
+		).css({'min-width': 150});
+		element.find('span').css({'min-width': 120, 'max-width': 120, 'display': 'inline-block'});
 
 		spyOn(jQuery.fn, 'shadowDraggable').and.callThrough();
 		element.appendTo('body');
 		underTest = element.nodeResizeWidget('1.a', mapModel, stagePositionForPointEvent);
+		textElement = element.find('span');
 	});
 	afterEach(function () {
 		element.detach();
@@ -37,19 +47,120 @@ describe('nodeResizeWidget', function () {
 			expect(jQuery.fn.shadowDraggable).toHaveBeenCalledOnJQueryObject(dragHandle);
 		});
 		describe('after the dragHandle has been dragged', function () {
-			var textElement;
 			beforeEach(function () {
-				textElement = element.find('span');
-				dragHandle.trigger(jQuery.Event('mm:start-dragging-shadow', {x: 10}));
+				dragHandle.trigger(jQuery.Event('mm:start-dragging-shadow', eventForX(120)));
 			});
-			it('should alter the min-width when the node is dragged right', function () {
-				dragHandle.trigger(jQuery.Event('mm:drag', {x: 20}));
-				expect(textElement.css('min-width')).toEqual('60px');
+			it('should alter the node min-width when the node is dragged right', function () {
+				dragHandle.trigger(jQuery.Event('mm:drag', eventForX(130)));
+				expect(underTest.css('min-width')).toEqual('130px');
 			});
-			it('should alter the min-width when the node is dragged right', function () {
-				dragHandle.trigger(jQuery.Event('mm:drag', {x: 30}));
-				expect(textElement.css('max-width')).toEqual('70px');
+
+			it('should alter the text area min-width when the node is dragged right', function () {
+				dragHandle.trigger(jQuery.Event('mm:drag', eventForX(130)));
+				expect(textElement.css('min-width')).toEqual('130px');
 			});
+			it('should alter the text area max-width when the node is dragged right', function () {
+				dragHandle.trigger(jQuery.Event('mm:drag', eventForX(130)));
+				expect(textElement.css('max-width')).toEqual('130px');
+			});
+			it('should alter the node min-width when the node is dragged left', function () {
+				dragHandle.trigger(jQuery.Event('mm:drag', eventForX(110)));
+				expect(textElement.css('min-width')).toEqual('110px');
+			});
+			it('should alter the text area min-width when the node is dragged left', function () {
+				dragHandle.trigger(jQuery.Event('mm:drag', eventForX(110)));
+				expect(textElement.css('min-width')).toEqual('110px');
+			});
+			it('should alter the text area max-width when the node is dragged left', function () {
+				dragHandle.trigger(jQuery.Event('mm:drag', eventForX(110)));
+				expect(textElement.css('max-width')).toEqual('110px');
+			});
+			describe('should enforce a minimum width', function () {
+				it('should alter the node min-width to 50 when the node is dragged left', function () {
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(20)));
+					expect(textElement.css('min-width')).toEqual('50px');
+				});
+				it('should alter the text area min-width to 50 when the node is dragged left', function () {
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(20)));
+					expect(textElement.css('min-width')).toEqual('50px');
+				});
+				it('should alter the text area max-width to 50 when the node is dragged left', function () {
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(20)));
+					expect(textElement.css('max-width')).toEqual('50px');
+				});
+			});
+			describe('should enforce min-width when node text is a very long unwrappable word', function () {
+				beforeEach(function () {
+					textElement.text('WWWWWWWWW');
+				});
+				it('should alter the node min-width  when the node is dragged left', function () {
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(20)));
+					expect(underTest.css('min-width')).toEqual('136px');
+				});
+				it('should alter the text area min-width when the node is dragged left', function () {
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(20)));
+					expect(textElement.css('min-width')).toEqual('136px');
+				});
+				it('should alter the text area max-width when the node is dragged left', function () {
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(20)));
+					expect(textElement.css('max-width')).toEqual('136px');
+				});
+			});
+			it('should call event stopPropagation', function () {
+				var evt = eventForX(20);
+				dragHandle.trigger(jQuery.Event('mm:drag', evt));
+				expect(evt.stopPropagation).toHaveBeenCalled();
+			});
+			it('should call event gesture stopPropagation', function () {
+				var evt = eventForX(20);
+				dragHandle.trigger(jQuery.Event('mm:drag', evt));
+				expect(evt.gesture.stopPropagation).toHaveBeenCalled();
+			});
+		});
+		[
+			['mm:stop-dragging', 'stopped'],
+			['mm:cancel-dragging', 'cancelled']
+		].forEach(function (args) {
+			describe('when dragging is ' + args[1], function () {
+				var evtX20;
+				beforeEach(function () {
+					evtX20 = eventForX(20);
+					dragHandle.trigger(jQuery.Event('mm:start-dragging-shadow', eventForX(120)));
+					dragHandle.trigger(jQuery.Event('mm:drag', eventForX(130)));
+				});
+				it('should call event stopPropagation', function () {
+					dragHandle.trigger(jQuery.Event(args[0], evtX20));
+					expect(evtX20.stopPropagation).toHaveBeenCalled();
+				});
+				it('should call event gesture stopPropagation', function () {
+					dragHandle.trigger(jQuery.Event(args[0], evtX20));
+					expect(evtX20.gesture.stopPropagation).toHaveBeenCalled();
+				});
+				it('should reset the node min-width to its initial value', function () {
+					dragHandle.trigger(jQuery.Event(args[0], evtX20));
+					expect(underTest.css('min-width')).toEqual('150px');
+				});
+				it('should reset the text area to its initial value', function () {
+					dragHandle.trigger(jQuery.Event(args[0], evtX20));
+					expect(textElement.css('min-width')).toEqual('120px');
+				});
+				it('should reset the text area max-width to its initial value', function () {
+					dragHandle.trigger(jQuery.Event(args[0], evtX20));
+					expect(textElement.css('max-width')).toEqual('120px');
+				});
+				it('should select node', function () {
+					dragHandle.trigger(jQuery.Event(args[0], eventForX(140)));
+					expect(mapModel.selectNode).toHaveBeenCalledWith('1.a');
+				});
+				it('should trigger an mm:resize event', function () {
+					var listener = jasmine.createSpy('listener');
+					underTest.on('mm:resize', listener);
+					dragHandle.trigger(jQuery.Event(args[0], eventForX(130)));
+					expect(listener).toHaveBeenCalled();
+					expect(listener.calls.mostRecent().args[0].nodeWidth).toEqual(130);
+				});
+			});
+
 		});
 	});
 });
