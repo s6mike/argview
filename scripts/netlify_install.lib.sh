471178d6bce1e96ff0652d7cd989a4c9606027f7
Michael Hayes <es6.mike@gmail.com>
2023-10-31 02:20:45 +0000

    Update `app_install()`

--- scripts/netlify_install.lib.sh
+++ scripts/netlify_install.lib.sh
@@ -5,13 +5,24 @@ echo "Running ${BASH_SOURCE[0]}"
 # aliases
 alias ai='app_install'
 
-# Replace echo with this where not piping output
 app_install() { # ai ./yq https://github.com/mikefarah/yq/releases/download/v4.30.8/yq_linux_amd64
-  app_path="$1"
+  download_dir="${1:-.}"
   app_url="$2"
-  mkdir -p "$(dirname "$app_path")"
-  wget -qO "$app_path" "$app_url"
-  chmod +x "$app_path"
+  wget -qP "$download_dir" "$app_url"
+  filename=${app_url##*/}
+  app_path="$download_dir/$filename"
+
+  # case "$filename" in
+  # *.tar.gz)
+  #   tar -zxf "$app_path"
+  #   app_path="${app_path%.*}"
+  #   ;;
+  # *)
+  #   chmod +x "$app_path"
+  #   ;;
+  # esac
+
+  echo "$app_path"
 }
 
 export -f app_install
