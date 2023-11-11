5714054f2b75c735b8b21a518feaaa9a4344a95c
Michael Hayes <es6.mike@gmail.com>
2023-11-11 03:01:51 +0000

    init: remove PATH update: break build if make fail

--- scripts/argmap_init_script.sh
+++ scripts/argmap_init_script.sh
@@ -68,7 +68,8 @@ CONDA_ENV_ARGMAP="$(getvar CONDA_ENV_ARGMAP)"
 #  QUESTION: Combine these?
 PATH_DIR_ARGMAP_SRC="$(getvar PATH_DIR_ARGMAP_SRC)"
 PATH_DIR_ARGMAP_LUA="$PATH_DIR_ARGMAP_SRC/lua"
-PATH="/home/linuxbrew/.linuxbrew/opt/lua@5.3/bin:$PATH_DIR_ARGMAP_LUA:$PATH"
+# PATH="/home/linuxbrew/.linuxbrew/opt/lua@5.3/bin:$PATH_DIR_ARGMAP_LUA:$PATH"
+PATH="$PATH_DIR_ARGMAP_LUA:$PATH"
 echo "PATH: $PATH"
 which lua
 
