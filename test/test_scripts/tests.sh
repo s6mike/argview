d7a7ea1a5d5e3ced165802057e0b26fee7cb67c0
Michael Hayes <es6.mike@gmail.com>
2023-11-12 23:50:44 +0000

    tests: build dependencies with make

--- test/test_scripts/tests.sh
+++ test/test_scripts/tests.sh
@@ -63,16 +63,14 @@ if [ "$1" != html ]; then
     __test a2t "$(getvar INPUT_FILE_YML)"     #3
     __test a2mu "$(getvar INPUT_FILE_YML)"    #4
 
-    make "${PATH_FILE_MAPJS_HTML_DIST_TAGS}" &&
-      # npx --prefix "$(getvar PATH_DIR_MAPJS_ROOT)" wait-on --timeout 10000 "${PATH_FILE_MAPJS_HTML_DIST_TAGS}" && # Waits for file to finish being generated before running tests
-      # create html file needed for testcafe and rendering tests
-      # Following will fail if run before webpack has generated html partial from src/mapjs, but wait-on should ensure that never happens
+    make "${PATH_FILE_MAPJS_HTML_DIST_TAGS}"
+    # npx --prefix "$(getvar PATH_DIR_MAPJS_ROOT)" wait-on --timeout 10000 "${PATH_FILE_MAPJS_HTML_DIST_TAGS}" && # Waits for file to finish being generated before running tests
+    # create html file needed for testcafe and rendering tests
+    # Following will fail if run before webpack has generated html partial from src/mapjs, but wait-on should ensure that never happens
 
-      #  TODO make these instead of 2hf:
-      # j2hf will still fail if json file missing, but it's part of repo so that shouldn't happen.
-      2hf -pq "$(getvar INPUT_FILE_JSON)"
-    2hf -pq "$(getvar INPUT_FILE_JSON2)"      # Dependency for recording PATH_REPLAY_SCRIPT_ADD_SUPPORTING_E2V3
-    2hf -pq "$(getvar INPUT_FILE_JSON_LINKS)" # Dependency for recording PATH_REPLAY_SCRIPT_EDIT_LINK_EXISTING
+    make "$(getvar PATH_OUTPUT_HTML_PUBLIC)/example1-clearly-false-white-swan-simplified.html"
+    make "$(getvar PATH_OUTPUT_HTML_PUBLIC)/example2-clearly-false-white-swan-v3.html"
+    make "$(getvar PATH_OUTPUT_HTML_PUBLIC)/example1-clearly-false-white-swan-simplified-with-links.html"
 
     ;;
   esac
