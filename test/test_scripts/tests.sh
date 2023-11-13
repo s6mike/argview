77662a6ba276ab190e9b49c7e380cac4e7a99346
Michael Hayes <es6.mike@gmail.com>
2023-11-13 01:11:22 +0000

    tests: netlify exit rather than return

--- test/test_scripts/tests.sh
+++ test/test_scripts/tests.sh
@@ -59,7 +59,7 @@ if [ "$1" != html ]; then
     # TEMP run this test only
     make "${PATH_FILE_MAPJS_HTML_DIST_TAGS}"
     __test make HTML_OPEN=true "$(getvar PATH_OUTPUT_HTML_PUBLIC)/example1-clearly-false-white-swan-simplified-1mapjs.html" #7/3
-    return
+    exit
     ;;
   *)
     __test luarocks lint "$(__find_rockspec)" #1 # Gets absolute path
