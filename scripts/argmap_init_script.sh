09f8be5aed12e00124917c1e9ec3265f9bd239c8
Michael Hayes <es6.mike@gmail.com>
2023-11-23 17:22:33 +0000

    init: remove brew cache location check

--- scripts/argmap_init_script.sh
+++ scripts/argmap_init_script.sh
@@ -16,7 +16,7 @@ init_config() {
   case $ENV in
 
   netlify)
-    brew --cache -s "lua@5.3"
+    # brew --cache -s "lua@5.3"
     # # PATH_CACHE=/opt/build/cache/argmap
     # # mkdir --parent "$PATH_CACHE"
     # # ls "$PATH_CACHE"
