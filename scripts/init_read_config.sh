cab2272c4ea569574386bbaae1514fc9bf665619
Michael Hayes <es6.mike@gmail.com>
2023-11-30 00:35:19 +0000

    23.2.2 fix(config): Use PATH_ARGMAP_ROOT:
    - Update to use PATH_ARGMAP_ROOT, PATH_LUA_ARGMAP and minor refinements:
      - `mapjs/config/config-mapjs-paths.yaml`
      - `scripts/init_read_config.sh`
      - `scripts/config_read_functions.lib.sh`
      - `mapjs/scripts/mapjs.env`
      - `src/lua/config_argmap.lua`
      - `src/lua/pandoc-argmap.lua`
      - `test/test_scripts/bash_aliases_argmap_test.sh`:
      Also prepare tests for future changes.
      - `test/test_scripts/tests.sh`

--- scripts/init_read_config.sh
+++ scripts/init_read_config.sh
@@ -13,53 +13,57 @@ main() {
 
   # QUESTION: Can I put min startup into separate environment file?
   #  Could then reference directly from makefile
-  export PATH_DIR_ARGMAP_ROOT="${PATH_DIR_ARGMAP_ROOT:-$WORKSPACE}"
+  # export PATH_ARGMAP_ROOT="${PATH_ARGMAP_ROOT:-$PATH_ARGMAP_ROOT}"
 
-  source "$PATH_DIR_ARGMAP_ROOT/scripts/config_read_functions.lib.sh"
+  source "$PATH_ARGMAP_ROOT/scripts/config_read_functions.lib.sh"
 
-  # TODO could move all env variablaes in this script to argmap.env file, after tidying that up
-  set -o allexport
-  PATH_FILE_ENV_ARGMAP="$PATH_DIR_ARGMAP_ROOT/config/environment-argmap.yaml"
-  PATH_FILE_ENV_ARGMAP_DEFAULTS="$PATH_DIR_ARGMAP_ROOT/config/environment-argmap-defaults.yaml"
-  PATH_FILE_CONFIG_ARGMAP_PATHS="$PATH_DIR_ARGMAP_ROOT/config/config-argmap-paths.yaml"
-  set +o allexport
+  # TODO could move all env variables in this script to argmap.env file, after tidying that up
+  # set -o allexport
+  # PATH_FILE_ENV_ARGMAP="$PATH_ARGMAP_ROOT/config/environment-argmap.yaml"
+  # PATH_FILE_ENV_ARGMAP_DEFAULTS="$PATH_ARGMAP_ROOT/config/environment-argmap-defaults.yaml"
+  # PATH_FILE_CONFIG_ARGMAP_PATHS="$PATH_ARGMAP_ROOT/config/config-argmap-paths.yaml"
+  # set +o allexport
 
   # In case env.yaml file doesn't exist, attempt to make a copy from default file, but without overwriting existing one.
   make "$PATH_FILE_ENV_ARGMAP"
 
-  __yaml2env "$PATH_FILE_ENV_ARGMAP" PORT_DEV_SERVER PATH_PROFILE_LOCAL
-  __yaml2env "$PATH_FILE_CONFIG_ARGMAP_PATHS" DIR_CONFIG KEYWORD_PROCESSED PATH_DIR_CONFIG_ARGMAP PATH_FILE_PANDOC_DEFAULT_CONFIG_PREPROCESSOR
+  __yaml2env "$PATH_FILE_ENV_ARGMAP" PORT_DEV_SERVER
+  __yaml2env "$PATH_FILE_CONFIG_ARGMAP_PATHS" PATH_FILE_PANDOC_DEFAULT_CONFIG_PREPROCESSOR
 
   # This part more complex now I have separated paths into two files.
   #   QUESTION: If I could combine them into one before processing perhaps it would be easier?
-  __yaml2env "$PATH_FILE_ENV_ARGMAP" DIR_MAPJS DIR_PUBLIC
+  __yaml2env "$PATH_FILE_ENV_ARGMAP" DIR_MAPJS DIR_PUBLIC PATH_ENVIRONMENT_GLOBAL PATH_BIN_GLOBAL
 
   # In case env file doesn't exist, attempt to make a copy from default file, but without overwriting existing one.
-  make "$PATH_DIR_ARGMAP_ROOT/$DIR_MAPJS/$DIR_CONFIG/environment-mapjs.yaml"
+  make "$PATH_ARGMAP_ROOT/$DIR_MAPJS/$DIR_CONFIG/environment-mapjs.yaml"
+
+  set -o allexport
 
   # Processes initial files needed for rest
-  set -o allexport
-  PATH_FILE_ENV_ARGMAP_PROCESSED="$PATH_DIR_CONFIG_ARGMAP/$KEYWORD_PROCESSED/environment-argmap-$KEYWORD_PROCESSED.yaml"
-  PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED="$PATH_DIR_CONFIG_ARGMAP/$KEYWORD_PROCESSED/config-argmap-paths-$KEYWORD_PROCESSED.yaml"
+  PATH_FILE_ENV_ARGMAP_PROCESSED="$PATH_DIR_CONFIG_ARGMAP_PROCESSED/environment-argmap-$KEYWORD_PROCESSED.yaml"
+  PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED="$PATH_DIR_CONFIG_ARGMAP_PROCESSED/config-argmap-paths-$KEYWORD_PROCESSED.yaml"
   set +o allexport
+
+  # make prints
 
   make "$PATH_FILE_ENV_ARGMAP_PROCESSED"
   make "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED"
 
-  __yaml2env "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED" PATH_DIR_CONFIG_MAPJS PATH_FILE_ENV_ARGMAP_PRIVATE
-
-  make "$PATH_FILE_ENV_ARGMAP_PRIVATE"
+  __yaml2env "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED" PATH_DIR_CONFIG_MAPJS
 
   # Can't use __yaml2env because it's not set to take the -l option
-  LIST_FILES_CONFIG_PROCESSED=$(__getvar_from_yaml -l LIST_FILES_CONFIG_PROCESSED "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED" "$PATH_FILE_ENV_ARGMAP_PROCESSED" | tr '\n' ' ')
+  # TODO though that might not matter now if I put this at the end:  | sed 's/^- //
+  LIST_FILES_CONFIG_PROCESSED=$(__getvar_from_yaml -e LIST_FILES_CONFIG_PROCESSED "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED" "$PATH_FILE_ENV_ARGMAP_PROCESSED" | tr '\n' ' ')
   export LIST_FILES_CONFIG_PROCESSED
+
+  # make prints
 
   # Process remaining config files
   #   QUESTION: Better to define above variables as part of make call instead of exporting them?
   make config # --warn-undefined-variables or -d for debugging
 
   # TODO cover this with make config
-  LIST_FILES_CONFIG_INPUT=$(__getvar_from_yaml -l LIST_FILES_CONFIG_INPUT "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED" "$PATH_FILE_ENV_ARGMAP_PROCESSED")
+  LIST_FILES_CONFIG_INPUT=$(__getvar_from_yaml LIST_FILES_CONFIG_INPUT "$PATH_FILE_CONFIG_ARGMAP_PATHS_PROCESSED" "$PATH_FILE_ENV_ARGMAP_PROCESSED")
   export LIST_FILES_CONFIG_INPUT
 }
 main "$@"
